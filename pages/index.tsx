0630c8d0eb7607ec74c2af2ba5ff3d213a11bcd1
Giovanni0788 <davidc11791@gmail.com>
2025-04-06 17:14:09 -0400

    Completed Modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,9 @@ tvShows,
   if (loading) return null
 
   return (
-    <div className="relitive h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+      showModal && '!h-screen overflow-hidden'
+    }`}>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
